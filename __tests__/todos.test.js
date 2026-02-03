3032de9b9020cb698ec4505cef615c9c68156fbf
Gerald C. Manatad <120861682+kekeke3@users.noreply.github.com>
2026-02-03 16:57:39 +0800

    Todo API with tests

---
+++ __tests__/todos.test.js
@@ -0,0 +1,19 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("Todo API", () => {
+  it("creates a todo", async () => {
+    const res = await request(app).post("/todos").send({ text: "Learn CI" });
+
+    expect(res.statusCode).toBe(201);
+    expect(res.body.text).toBe("Learn CI");
+  });
+
+  it("lists todos", async () => {
+    const res = await request(app).get("/todos");
+
+    expect(res.statusCode).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+  });
+  
+});
